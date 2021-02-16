5cc6141d99a41e93e61595346ec82b1110fcb5f1
Chris Chasm <chris@chasm.solutions>
2021-02-16 09:08:38 -0700

    Initialized

---
+++ dt-demos-analytics.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: DT Demos Analytics Code
+ * Plugin URI: https://github.com/ChrisChasm/dt_demos_analytics
+ * Description: Connect this Disciple Tools site to a larger network of sites. Adds security sensitive totals, mapping, activity logging.
+ */
+
+add_action( 'wp_head', function(){
+    ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2NQW0WF4Q8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2NQW0WF4Q8');
+    </script>
+    <?php
+});
